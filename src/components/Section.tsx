93f2b0f5036b9bee0a138a391da5c486c346bbcc
Jason Lee <jason.e.lee8@gmail.com>
2025-07-13 19:59:44 -0400

    fix padding issue with section headers

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,8 +5,8 @@ type SectionProps = {
 };
 
 const Section = ({ children, header, id }: SectionProps) => (
-  <div className="w-full h-full flex flex-col items-start mb-10 xl:mb-20 pt-20" id={id}>
-    <h3 className="w-full text-transparent bg-clip-text bg-gradient-to-t from-blue-400 to-blue-300 text-5xl md:text-7xl font-bold mb-10">{header}</h3>
+  <div className="w-full h-full flex flex-col items-start mb-10 xl:mb-10 pt-20" id={id}>
+    <h3 className="w-full text-transparent bg-clip-text bg-gradient-to-t from-blue-400 to-blue-300 text-5xl md:text-7xl font-bold py-5 mb-5">{header}</h3>
     {children}
   </div>
 );
