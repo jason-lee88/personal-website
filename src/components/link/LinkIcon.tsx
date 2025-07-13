fe9f3f5fa426864837b7c910e3746a8277f65ecd
Jason Lee <jason.e.lee8@gmail.com>
2025-07-13 18:49:36 -0400

    icon hover color

--- src/components/link/LinkIcon.tsx
+++ src/components/link/LinkIcon.tsx
@@ -4,7 +4,7 @@ type IconProps = {
 }
 
 const LinkIcon = ({ icon, href }: IconProps) => (
-  <a className="w-10 h-10 bg-neutral-500/40 rounded-xl flex justify-center items-center text-neutral-50" href={href} target="_blank">
+  <a className="w-10 h-10 bg-neutral-500/40 hover:bg-sky-500/100 transition ease-in-out duration-300 rounded-xl flex justify-center items-center text-neutral-50" href={href} target="_blank">
     {icon}
   </a>
 );
--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -7,7 +7,7 @@ type NavLinkProps = {
 };
 
 const NavLink = ({ href, label, active }: PropsWithChildren<NavLinkProps>) => (
-  <a className={`${active ? "text-neutral-50" : "text-neutral-500"} hover:text-neutral-50 transition ease-in-out select-none text-lg font-bold`} href={href}><span className="mr-4 h-px w-8" />{label.toUpperCase()}</a>
+  <a className={`${active ? "text-neutral-50" : "text-neutral-500"} hover:text-neutral-50 transition ease-in-out duration-300 select-none text-lg font-bold`} href={href}><span className="mr-4 h-px w-8" />{label.toUpperCase()}</a>
 );
 
 export default NavLink;
